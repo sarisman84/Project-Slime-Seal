8d4114db5590dee3967991df9e0c9a11c07309e4
Spyro <epicdudeness677@gmail.com>
2021-01-23 13:22:28 +0100

    Bridge Mechanic: Added Animation

--- Assets/Scripts/Interactivity/BridgeController.cs
+++ Assets/Scripts/Interactivity/BridgeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DG.Tweening;
 using Player;
 using Unity.Mathematics;
 using UnityEngine;
@@ -9,9 +10,11 @@ namespace Interactivity
     {
         public float minSizeRequirement = 2.5f;
         public float bridgeWidthSize = 3f;
+        public float bridgeSpawnSpeed = 2f;
         public GameObject bridgeModelPrefab;
-
+        [SerializeField] private Ease bridgeEaseType;
         public Vector3 waypointA, waypointB;
+       
 
         public void BuildBridge(Collider col)
         {
@@ -31,10 +34,15 @@ namespace Interactivity
         {
             var transform2 = transform;
             GameObject obj = Instantiate(bridgeModelPrefab, transform);
-            obj.transform.position = waypointA + (midPoint);
-            obj.transform.localRotation = quaternion.LookRotation(midPoint.normalized, Vector3.up);
-            obj.transform.localScale = obj.transform.localScale + (transform2.forward * midPoint.magnitude * 2f) +
-                                       transform2.right * width;
+            obj.transform.position = waypointA;
+            obj.transform.localScale = Vector3.one;
+            obj.transform.DOMove(waypointA + (midPoint), bridgeSpawnSpeed).OnStart(() =>
+            {
+                obj.transform.localRotation = quaternion.LookRotation(midPoint.normalized, Vector3.up);
+                obj.transform.localScale += transform2.right * width;
+                obj.transform.DOScale(obj.transform.localScale + (transform2.forward * midPoint.magnitude * 2f),
+                    bridgeSpawnSpeed);
+            }).SetEase(bridgeEaseType);
         }
 
 
