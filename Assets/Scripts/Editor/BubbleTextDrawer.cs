29a02cdb24033a9d58ed4b44563896f9f5ac10f4
Spyro <epicdudeness677@gmail.com>
2021-01-25 22:39:41 +0100

    Auto stash before merge of "design/CollisionFix" and "design/NewMeme"

--- Assets/Scripts/Editor/BubbleTextDrawer.cs
+++ Assets/Scripts/Editor/BubbleTextDrawer.cs
@@ -1,20 +1,100 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-namespace Editor
-{
-    [CustomPropertyDrawer(typeof(TYPE))]
-    public class BubbleTextDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property,
-            GUIContent label)
-        {
-            
-        }
-
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            return base.GetPropertyHeight(property, label);
-        }
-    }
-}
+﻿// using Interactivity;
+// using TPUModelerEditor;
+// using UnityEditor;
+// using UnityEngine;
+//
+// namespace Editor
+// {
+//     [CustomPropertyDrawer(typeof(BubbleText.BubbleUI))]
+//     public class BubbleTextDrawer : PropertyDrawer
+//     {
+//         private bool m_ValDur, m_ValTrans, m_ValEvent;
+//
+//         public override void OnGUI(Rect position, SerializedProperty property,
+//             GUIContent label)
+//         {
+//             Rect r = position;
+//             SerializedProperty it = property.serializedObject.GetIterator();
+//             it.Next(true);
+//             bool disableFirstElement = true;
+//             while (it.NextVisible(false))
+//             {
+//                 EditorGUI.BeginDisabledGroup(disableFirstElement);
+//                 r.height = EditorGUI.GetPropertyHeight(it, it.isExpanded);
+//                 switch (it.propertyPath)
+//                 {
+//                     case "hasDuration":
+//                     case "hasTransition":
+//                     case "useCustomEvents":
+//                         it.boolValue = EditorGUI.Toggle(r, new GUIContent(it.displayName), it.boolValue,
+//                             GUIStyles.defaultButtonStyle);
+//                         StoreBoolean(it.Copy());
+//                         continue;
+//
+//                     case "displayDuration":
+//                     case "transitionTime":
+//                     case "onEnableUIElement":
+//                     case "onDisableUIElement":
+//                         if (GetBoolean(it.Copy()))
+//                             EditorGUI.PropertyField(r, it, new GUIContent(it.displayName), false);
+//                         continue;
+//                 }
+//
+//                 EditorGUI.PropertyField(r, it, new GUIContent(it.displayName), false);
+//                 r.y += r.height;
+//                 EditorGUI.EndDisabledGroup();
+//                 disableFirstElement = false;
+//             }
+//         }
+//
+//         private void DrawProperty(Rect r, SerializedProperty it)
+//         {
+//         }
+//
+//         private bool GetBoolean(SerializedProperty it)
+//         {
+//             switch (it.propertyPath)
+//             {
+//                 case "hasDuration":
+//                     return m_ValDur;
+//                 case "hasTransition":
+//                     return m_ValTrans;
+//                 case "useCustomEvents":
+//                     return m_ValEvent;
+//             }
+//
+//             return false;
+//         }
+//
+//         private void StoreBoolean(SerializedProperty it)
+//         {
+//             switch (it.propertyPath)
+//             {
+//                 case "hasDuration":
+//                     m_ValDur = it.boolValue;
+//                     return;
+//                 case "hasTransition":
+//                     m_ValTrans = it.boolValue;
+//                     return;
+//                 case "useCustomEvents":
+//                     m_ValEvent = it.boolValue;
+//                     break;
+//             }
+//         }
+//
+//
+//         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+//         {
+//             float totalHeight = base.GetPropertyHeight(property, label);
+//
+//             SerializedProperty it = property.serializedObject.GetIterator();
+//             it.Next(true);
+//             while (it.NextVisible(false))
+//             {
+//                 totalHeight += EditorGUI.GetPropertyHeight(it, it.isExpanded);
+//             }
+//
+//             return totalHeight;
+//         }
+//     }
+// }
